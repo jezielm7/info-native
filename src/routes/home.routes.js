c7d6e16571e962607800b56f300e17b324a54950
Jeziel Marques <jezielmarques7@gmail.com>
2020-03-13 16:20:34 -0300

    styled bottom navBar

--- src/routes/home.routes.js
+++ src/routes/home.routes.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Home from '../pages/Home';
-import Login from '../pages/Login';
 import Content from '../pages/Content';
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 export default function HomeRoutes() {
   return (
-      <Tab.Navigator initialRouteName="Home" >
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Content" component={Content} />
-      </Tab.Navigator>
+    <Tab.Navigator
+      initialRouteName="Home"
+      activeColor="#fff"
+      inactiveColor="#eee"
+      barStyle={{ backgroundColor: '#7159c1' }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ focused }) => (
+            <Icon name="home" size={25} color={focused ? '#fff' : '#ddd'} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Content"
+        component={Content}
+        options={{
+          tabBarLabel: 'Content',
+          tabBarIcon: ({ focused }) => (
+            <Icon name="content-paste" size={25} color={focused ? '#fff' : '#ddd'} />
+          ),
+        tabBarOptions: { showLabel: false }
+        }}
+      /> 
+    </Tab.Navigator>
   );
 }
